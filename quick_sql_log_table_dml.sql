e49e7776635f70d9a8855679f3e316b9c4631b23
Orhan Çavuş <orhancv@gmail.com>
2021-10-10 13:45:53 +0300

    Update quick_sql_log_table_dml.sql
    Table DML added

--- quick_sql_log_table_dml.sql
+++ quick_sql_log_table_dml.sql
@@ -5,3 +5,32 @@ log_table
   change_date    date/nn   -- last modificationd datetime of record
   message        vc200     -- additional message
 
+create table log_table (
+    id                             number not null constraint log_table_id_pk primary key,
+    table_name                     varchar2(50) not null,
+    table_pk                       varchar2(100) not null,
+    dml_type                       varchar2(1) not null,
+    change_date                    date not null,
+    message                        varchar2(200)
+);
+
+create or replace trigger log_table_biu
+    before insert or update 
+    on log_table
+    for each row
+begin
+    if :new.id is null then
+        :new.id := to_number(sys_guid(), 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+    end if;
+end log_table_biu;
+/
+
+comment on column log_table.change_date is 'last modificationd datetime of record';
+
+comment on column log_table.dml_type is '''I'' - insert, ''U'' - update, ''D'' - delete';
+
+comment on column log_table.message is 'additional message';
+
+comment on column log_table.table_name is 'table name';
+
+comment on column log_table.table_pk is 'table primary key of table beeing logged';
